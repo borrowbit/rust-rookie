d1ee9927f987395cbc2163d175da46d8f39a772a
Dmitry Smirnov <dima7mirnoff@gmail.com>
2025-11-08 10:53:56 +0300

    add other formsts

--- lesson-2/src/main.rs
+++ lesson-2/src/main.rs
@@ -21,8 +21,56 @@ fn fuzz_buzz(value: i32) {
     }
 }
 
+fn format_data() {
+    let year = 2024;
+    let month = 1;
+    let day = 15;
+
+    println!("📅 {}-{:02}-{:02}", year, month, day)
+}
+
+fn format_money() {
+    let amount = 142.1211;
+
+    println!("💵 {:.2} ₽", amount)
+}
+
+fn format_rgb() {
+    let r = 255;
+    let g = 128;
+    let b = 0;
+
+    println!("🎨 #{:02X}{:02X}{:02X}", r, g, b)
+}
+
+fn format_table() {
+    let name = "Alice";
+    let age = 25;
+    let scope = 95.543;
+
+    println!("| {:<8} | {:^8} | {:>8.1} | ", name, age, scope)
+}
+
 fn main() {
-    for n in 1..=20 {
-        fuzz_buzz(n);
-    }
+    let divider = "-------------";
+
+    // 1. FuzzBuzz
+    fuzz_buzz(5);
+    println!("{}", divider);
+
+    // 2. Format data
+    format_data();
+    println!("{}", divider);
+
+    // 2. Format money
+    format_money();
+    println!("{}", divider);
+
+    // 2. Format rgb
+    format_rgb();
+    println!("{}", divider);
+
+    // 2. Format table
+    format_table();
+    println!("{}", divider);
 }
